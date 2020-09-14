cd86420e48355a2ccd66fa5301c9bb3ac3898f9d
Igor Balteiro <igor.balteiro@ibm.com>
2020-09-14 17:56:55 -0300

    Add patients component into app component

--- prontuario-eletronico-web/src/App.test.tsx
+++ prontuario-eletronico-web/src/App.test.tsx
@@ -2,8 +2,34 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const appTitle = getByText(/prontomed/i);
+
+  expect(appTitle).toBeInTheDocument();
+});
+
+test('renders patients link', () => {
+  const { getAllByText } = render(<App />);
+
+  const patientsLink = getAllByText(/pacientes/i);
+
+  expect(patientsLink.length).toBe(2);
+});
+
+test('renders schedules link', () => {
+  const { getByText } = render(<App />);
+
+  const schedulesLink = getByText(/agendamentos/i);
+
+  expect(schedulesLink).toBeInTheDocument();
+});
+
+test('renders patients component', () => {
+  const { getByText } = render(<App />);
+
+  const patientsComponent = getByText(/novo paciente/i);
+
+  expect(patientsComponent).toBeInTheDocument();
 });
--- prontuario-eletronico-web/src/App.tsx
+++ prontuario-eletronico-web/src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Patients from './components/Patients/Patients';
+
+const patientsData = [
+  {
+    name: 'Pikachu',
+    gender: 'no gender',
+    birthDate: 'unknown birthdate',
+    telephone: 'no telephone'
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className='App'>
+      <header className='App-header'>
+        <h1 className='App-header-title'>PRONTOMED</h1>
+        <h2>Pacientes</h2>
+        <h2>Agendamentos</h2>
       </header>
-    </div>
+      <div>
+        <Patients patientsListData={patientsData} />
+      </div>
+    </main>
   );
 }
 
