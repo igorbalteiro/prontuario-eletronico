537dbe81949d306bbf9e868f22b8b5a0630a0706
Igor Balteiro <igor.balteiro40@gmail.com>
2020-10-04 12:23:13 -0300

    Add swagger documentation

--- prontuario-eletronico-api/server.js
+++ prontuario-eletronico-api/server.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -12,6 +13,9 @@ app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const swaggerDocument = require('./swagger.json');
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // simple route
 app.get('/', (req, res) => {
